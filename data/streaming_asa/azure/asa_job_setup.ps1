0812486297d56d939ea986e80e2ba168f4228b71
Nithin M <nimadha@microsoft.com>
2018-06-12 10:27:12 -0700

    interim commit

--- data/streaming_asa/azure/asa_job_setup.ps1
+++ data/streaming_asa/azure/asa_job_setup.ps1
@@ -1,18 +1,15 @@
 
-
+# function to create a global dictionary that gets referrenced later
 function SetGlobalParams  {
-
     # dictionary object to load varaibles that will be input to asa job
     $global:dict  =  Get-Content .\asa_job_inputs.json  | ConvertFrom-Json
-
-
 }
 
+# updates the asa params in the asa job json file
 function UpdateAsaParams {
 
     $hashTable = $global:dict.asajobinputs 
     $hashTable.GetEnumerator() | ForEach-Object {
-
         $nameToReplace = -join("<",$_.Name,">")
         (Get-Content .\asa_job.json) -replace  $nameToReplace , $_.Value | 
         Set-Content .\asa_job.json
