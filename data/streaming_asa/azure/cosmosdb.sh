e8ac4017f94a34fd716c844a9c0a4ac58aa2e231
Nithin Madhavanpillai <nimadha@microsoft.com>
2018-06-28 11:11:28 -0700

    scripted the chages to add cosmos db at output

--- data/streaming_asa/azure/cosmosdb.sh
+++ data/streaming_asa/azure/cosmosdb.sh
@@ -3,24 +3,42 @@
 
 
 # Set variables for the new account, database, and collection
-resourceGroupName='asa-ra-1-stream1'
-location='southcentralus'
-name='docdb-taxicab'
-databaseName='docdb-taxicab-database'
-collectionName='docdb-taxicabavgtrippermile-collection'
+resourceGroup='asa-ra-2-test'
+resourceLocation='southcentralus'
+cosmosDatabaseAccount='docdb-taxi-asa-ra-2-test'
+cosmosDatabase='docdb-taxicab-database'
+cosmosDataBaseCollection='docdb-taxicabavgtrippermile-collection'
+eventHubNamespace='asa-ra-2-test-testeventhub'
 
+echo "resource group = $resourceGroup"
+echo "resourceLocation = $resourceLocation"
+echo "cosmosDatabaseAccount = $cosmosDatabaseAccount"
+echo "cosmosDatabase = $cosmosDatabase"
+echo "cosmosDataBaseCollection = $cosmosDataBaseCollection"
+
+
+
+
+echo "creating resource group $resourceGroup"
 # Create a resource group
-az group create --name $resourceGroupName --location $location
+az group create --name $resourceGroup --location $resourceLocation
 
+
+echo "creating cosmos account $cosmosDatabaseAccount"
 # Create a DocumentDB API Cosmos DB account
-az cosmosdb create --name $name --kind GlobalDocumentDB --resource-group $resourceGroupName --max-interval 10 --max-staleness-prefix 200 
+az cosmosdb create --name $cosmosDatabaseAccount --kind GlobalDocumentDB --resource-group $resourceGroup --max-interval 10 --max-staleness-prefix 200 
 
+
+echo "creating cosmos db $cosmosDatabase"
 # Create a database 
-az cosmosdb database create --name $name --db-name $databaseName --resource-group $resourceGroupName
+az cosmosdb database create --name $cosmosDatabaseAccount --db-name $cosmosDatabase --resource-group $resourceGroup
 
+
+echo "creating cosmos collection $cosmosDataBaseCollection"
 # Create a collection
-az cosmosdb collection create --collection-name $collectionName --name $name --db-name $databaseName --resource-group $resourceGroupName
+az cosmosdb collection create --collection-name $cosmosDataBaseCollection --name $cosmosDatabaseAccount --db-name $cosmosDatabase --resource-group $resourceGroup
 
 
+echo "creating asa job"
 # Create 2 event hub , one storage account and a asa job
-az group deployment create --resource-group $resourceGroupName --template-file ./deploy.json --parameters eventHubNamespace="taxi" outputStorageAccountName="taxi"
+az group deployment create --resource-group $resourceGroup --template-file ./deploy.json --parameters eventHubNamespace=$eventHubNamespace outputCosmosDatabaseAccount=$cosmosDatabaseAccount outputCosmosDatabase=$cosmosDatabase outputCosmosDatabaseCollection=$cosmosDataBaseCollection
